797f8f34c10c744f5c66319c1313c5a076c635e0
andres <dandresleon64@gmail.com>
2024-08-01 22:10:47 -0500

    ajustes

--- mjolnir_arq/module.py
+++ mjolnir_arq/module.py
@@ -1,13 +1,25 @@
-
+import pyfiglet
 from InquirerPy import inquirer
+from termcolor import colored
+
+def show_title():
+    title = pyfiglet.figlet_format("MJOLNIR-ARQ")
+    title = colored(title, 'cyan')
+    print(colored(".", 'cyan') * 100)
+    print(title)
+    print(colored("mjolnir-arq: 0.0.4", 'cyan'))
+    print(colored("Python: 3.11", 'cyan'))
+    print(colored("Author: Marlon Andres Leon Leon", 'cyan'))
+    print(colored(".", 'cyan') * 100)
+
+def option1():
+    name = inquirer.text(message="Introduce tu nombre:").execute()
+    print(f"Has introducido: {name}")
+
 
 def main():
-    options = [
-        "Opción 1",
-        "Opción 2",
-        "Opción 3",
-        "Salir"
-    ]
+    show_title()
+    options = ["Opción 1", "Opción 2", "Opción 3", "Salir"]
 
     selected_option = inquirer.select(
         message="Seleccione una opción:",
@@ -16,7 +28,7 @@ def main():
     ).execute()
 
     if selected_option == "Opción 1":
-        print("Has seleccionado la Opción 1.")
+        option1()
     elif selected_option == "Opción 2":
         print("Has seleccionado la Opción 2.")
     elif selected_option == "Opción 3":
@@ -25,3 +37,5 @@ def main():
         print("Saliendo...")
 
 
+if __name__ == "__main__":
+    main()
