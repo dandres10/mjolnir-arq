3d8e2057eaa8e27c9e8f3223355ae19e1caa15e6
andres <dandresleon64@gmail.com>
2024-08-05 21:03:39 -0500

    domain_models_entities

--- mjolnir_arq/business/mjolnir_business.py
+++ mjolnir_arq/business/mjolnir_business.py
@@ -47,13 +47,21 @@ class MjolnirBusiness:
     def create_flow_base(self):
         self.db = ConnectionPostgresql(loginDB=self.data_connection_db())
         name_table = inquirer.text(message="Introduce nombre de la tabla:").execute()
+
         result = self.validate_name_table(name_table=name_table)
         if not result:
             return
-        result = self.directory_exists_entities(name_table=name_table)
+
+        result = self.domain_models_entities(name_table=name_table)
         if not result:
             return
+
+    def domain_models_entities(self, name_table: str):
+        result = self.directory_exists_entities(name_table=name_table)
+        if not result:
+            return False
         self.create_directory_entity(name_table=name_table)
+
         entity_base = self.create_entity_base(name_table=name_table)
         self.file_manager.create_file(
             file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/{name_table}.py",
@@ -64,6 +72,26 @@ class MjolnirBusiness:
             file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/{name_table}_save.py",
             content=entity_save,
         )
+        entity_read = self.create_entity_read(name_table=name_table)
+        self.file_manager.create_file(
+            file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/{name_table}_read.py",
+            content=entity_read,
+        )
+        entity_delete = self.create_entity_delete(name_table=name_table)
+        self.file_manager.create_file(
+            file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/{name_table}_delete.py",
+            content=entity_delete,
+        )
+        entity_update = self.create_entity_update(name_table=name_table)
+        self.file_manager.create_file(
+            file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/{name_table}_update.py",
+            content=entity_update,
+        )
+        self.file_manager.create_file(
+            file_path=f"{self.current_directory}/src/domain/models/entities/{name_table}/__init__.py"
+        )
+
+        return True
 
     def validate_name_table(self, name_table: str):
         table_names = self.db.inspector.get_table_names()
@@ -103,7 +131,7 @@ class MjolnirBusiness:
         model_code = f"from pydantic import BaseModel, Field, UUID4\n"
         model_code += f"from typing import Optional\n"
         model_code += f"from datetime import datetime\n\n"
-        model_code += f"class {name_table}(BaseModel):\n"
+        model_code += f"class {snake_to_pascal(snake_str=name_table)}(BaseModel):\n"
         columns = self.db.inspector.get_columns(name_table)
         for column in columns:
             name = column["name"]
@@ -142,10 +170,76 @@ class MjolnirBusiness:
 
         return model_code
 
+    def create_entity_update(self, name_table: str):
+        model_code = f"from pydantic import BaseModel, Field, UUID4\n"
+        model_code += f"from typing import Optional\n"
+        model_code += f"from datetime import datetime\n\n"
+        model_code += (
+            f"class {snake_to_pascal(snake_str=name_table)}Update(BaseModel):\n"
+        )
+        columns = self.db.inspector.get_columns(name_table)
+        for column in columns:
+            name = column["name"]
+            column_type = column["type"]
+            nullable = column["nullable"]
+            default = column.get("default")
+
+            exclude_fields = ["created_date", "updated_date"]
+
+            if not name in exclude_fields:
+                model_code += f"    {name}: {self.create_fields_update(column_type=column_type, name=name, nullable=nullable, default=default)}\n"
+
+        return model_code
+
+    def create_entity_read(self, name_table: str):
+        model_code = (
+            f"from pydantic import UUID4, BaseModel, field_validator, Field\n\n"
+        )
+        model_code += f"class {snake_to_pascal(snake_str=name_table)}Read(BaseModel):\n"
+        model_code += f"    id: UUID4 = Field(...)\n"
+
+        return model_code
+
+    def create_entity_delete(self, name_table: str):
+        model_code = (
+            f"from pydantic import UUID4, BaseModel, field_validator, Field\n\n"
+        )
+        model_code += (
+            f"class {snake_to_pascal(snake_str=name_table)}Delete(BaseModel):\n"
+        )
+        model_code += f"    id: UUID4 = Field(...)\n"
+
+        return model_code
+
     def create_fields(self, column_type: Any, name: Any, nullable: Any, default: Any):
         if isinstance(column_type, UUID):
             field_type = "Optional[UUID4]"
             field_default = "Field(default=None)"
+        elif isinstance(column_type, VARCHAR):
+            field_type = "Optional[str]" if nullable else "str"
+            max_length = column_type.length
+            if nullable:
+                field_default = f"Field(default=None, max_length={max_length})"
+            else:
+                field_default = f"Field(..., max_length={max_length})"
+        elif isinstance(column_type, BOOLEAN):
+            field_type = "bool"
+            field_default = f"Field(default={default == 'true'})"
+        elif isinstance(column_type, TIMESTAMP):
+            field_type = "Optional[datetime]"
+            field_default = "Field(default_factory=datetime.now)"
+        else:
+            field_type = "str"
+            field_default = "Field(default=None)"
+
+        return f"{field_type} = {field_default}"
+
+    def create_fields_update(
+        self, column_type: Any, name: Any, nullable: Any, default: Any
+    ):
+        if isinstance(column_type, UUID):
+            field_type = "UUID4"
+            field_default = "Field(...)"
         elif isinstance(column_type, VARCHAR):
             field_type = "Optional[str]" if nullable else "str"
             max_length = column_type.length
